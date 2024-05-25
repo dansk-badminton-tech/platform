50f73182a442ea978fa9826e94876676a1dc7d39
Daniel Fly Nygaard <dfn@veo.co>
2024-05-25 11:14:13 +0200

    Updated database

--- merge-secret.sh
+++ merge-secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl create secret -n nembadminton generic nembadminton --dry-run=client --from-literal=PUSHER_APP_SECRET=password -o json | kubeseal --cert ~/projects/nembadminton-infrastructure/infrastructure/sealed-secrets/pub-sealed-secrets.pem -o yaml --merge-into production/nembadminton-secrets-sealed.yaml
+kubectl create secret -n nembadminton generic nembadminton --dry-run=client --from-literal=DB_PASSWORD=password -o json | kubeseal --cert ./pub-sealed-secrets.pem -o yaml --merge-into production/nembadminton-secrets-sealed.yaml
